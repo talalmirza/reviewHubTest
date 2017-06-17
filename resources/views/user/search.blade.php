45781af59a0aeed02a5f584dbf736ea14651e991
GeOyAli <alihaiderbabar0@gmail.com>
2017-06-17 10:39:29 -0700

    commit3
    commit3 aj

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -18,15 +18,14 @@ a#sub:visited{
     border-top-width: 0px;border-left-width: 0px;border-right-width: 0px;
 }
 
+#card{box-shadow:0 6px 6px 0 rgba(0,0,0,0.80),0 6px 10px 0 rgba(0,0,0,0.80)}
 a#everything:hover{
     border-color: rgb(1, 1, 41);
     background-color:rgb(1, 1, 41);
     color:white;
     border-top-width: 0px;border-left-width: 0px;border-right-width: 0px;
 }
-img#food{
-    border-radius:50%;
-}
+
 </style>
 @endsection
 
@@ -36,45 +35,44 @@ img#food{
         <div class="row">
             <div class="row"><h1>Search results for "Ali"</h1></div>
             <div class="row">
-                <div class="col-md-2 col-sm-2" style="background-color: #0F0F0F; padding-top: 20px">
-                    <div class="row" id="navtabs_row" style="background-color: #0F0F0F;">
-                        <div class="col-md-12 col-sm-12" style="padding-bottom: 20px;padding-left: 0px;padding-right: 0px;">
-                            <ul class="nav nav-tabs nav-stacked" id="nav"style="background-color: #0F0F0F;border-bottom: 0px">
-                                <li role="presentation" id="dash" class="active"><a class="text-centre" style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="everything" data-toggle="tab" href="#home">Everything</a></li>{{--<span id="span" style="font-size:small; left:47%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                                <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="sub"data-toggle="tab" href="#subslist">Posts</a></li>{{--<span id="span" style="font-size: small; left:68%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                                <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;"id="sub" data-toggle="tab" href="#category_list">Tags</a></li>{{--<span id="span" style="font-size: small; left:73%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                            </ul>
+
+                    <div class="col-md-2 col-sm-2 w3-card w3-darkgrey" id="card" style=" background-color: #0F0F0F; padding-top: 20px;margin-top: 30px;">
+                        <div class="row" id="navtabs_row" style="background-color: #0F0F0F;">
+                            <div class="col-md-12 col-sm-12" style="padding-bottom: 20px;padding-left: 0px;padding-right: 0px;">
+                                <ul class="nav nav-tabs nav-stacked" id="nav"style="background-color: #0F0F0F;border-bottom: 0px">
+                                    <li role="presentation" id="dash" class="active"><a class="text-centre" style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="everything" data-toggle="tab" href="#home">Everything</a></li>{{--<span id="span" style="font-size:small; left:47%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                                    <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="sub"data-toggle="tab" href="#subslist">Posts</a></li>{{--<span id="span" style="font-size: small; left:68%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                                    <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;"id="sub" data-toggle="tab" href="#category_list">Tags</a></li>{{--<span id="span" style="font-size: small; left:73%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
                 <div class="col-md-8 col-sm-8 {{--col-md-offset-1 col-sm-offset-1--}}">
                     <div class="tab-content">
                         <div id="home" class="tab-pane fade in active">
                             <div class="row">
-                                <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;">
+                                <div class="col-md-10 col-sm-10" style="margin-bottom: 30px;">
                                     <!-- Media middle -->
-                                    <div class="media">
+
                                         @include ('user.partials._postbox')
-                                    </div>
-                                    </div>
+
                                 </div>
+
+                            </div>
+
                                 <hr style="border-top:5px solid #eee">
                         </div>
                             {{--@include ('user.partials._postbox')--}}
                         <div id="category_list" class="tab-pane fade in fixed-bottom">
-                            <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;padding-left:0px;">
+                            <div class="col-md-11 col-sm-11" style="margin-bottom: 30px;padding-left:0px;">
                                     <!-- Media middle -->
-                                <div class="media">
-                                    @include ('user.partials._postbox')
-                                </div>
+                                @include ('user.partials._postbox')
                             </div>
                         </div>
                         <div id="subslist" class="tab-pane fade in">
-                            <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;padding-left:0px;">
+                            <div class="col-md-11 col-sm-11" style="margin-bottom: 30px;padding-left:0px;">
                                     <!-- Media middle -->
-                                <div class="media">
-                                    @include ('user.partials._postbox')
-                                </div>
+                                @include ('user.partials._postbox')
                             </div>
                         </div>
                     </div>
