23cc092fcec50def638855de78682feec875ea41
GeOyAli <alihaiderbabar0@gmail.com>
2017-06-17 08:53:06 -0700

    commit 1
    addition

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -3,10 +3,10 @@
 
 @section('custom-css')
 <style>
-
-ul.nav{
- background-color: #0F0F0F;
+#everything{
+    color: white;
 }
+
 li#dash{
     width: 101%;
     text-align:center;
@@ -32,152 +32,56 @@ img#food{
 
 @section ('content')
     @include ('user.partials.navbar')
-
-        <div class="container" style="background-color: white">
+    <div class="container" style="background-color: white">
+        <div class="row">
+            <div class="row"><h1>Search results for "Ali"</h1></div>
             <div class="row">
-                <div class="row"><h1>Search results for "Ali"</h1></div>
-                <div class="row">
-                    <div class="col-md-2 col-sm-2" style="background-color: #0F0F0F; padding-top: 20px">
-                        <div class="row" id="navtabs_row" style="background-color: #0F0F0F;">
-
-                            <div class="col-md-12 col-sm-12" style="padding-bottom: 20px;padding-left: 0px;padding-right: 0px;">
-                                <ul class="nav nav-tabs nav-stacked" id="nav"style="background-color: #0F0F0F;border-bottom: 0px">
-                                    <li role="presentation" id="dash" class="active"><a class="text-centre" style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="everything" data-toggle="tab" href="#home">Everything</a></li>{{--<span id="span" style="font-size:small; left:47%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                                    <li role="presentation" id="dash">               <a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="sub"data-toggle="tab" href="#subslist">Posts</a></li>{{--<span id="span" style="font-size: small; left:68%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                                    <li role="presentation" id="dash">               <a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;"id="sub" data-toggle="tab" href="#category_list">Tags</a></li>{{--<span id="span" style="font-size: small; left:73%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
-                                </ul>
-                             </div>
-
+                <div class="col-md-2 col-sm-2" style="background-color: #0F0F0F; padding-top: 20px">
+                    <div class="row" id="navtabs_row" style="background-color: #0F0F0F;">
+                        <div class="col-md-12 col-sm-12" style="padding-bottom: 20px;padding-left: 0px;padding-right: 0px;">
+                            <ul class="nav nav-tabs nav-stacked" id="nav"style="background-color: #0F0F0F;border-bottom: 0px">
+                                <li role="presentation" id="dash" class="active"><a class="text-centre" style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="everything" data-toggle="tab" href="#home">Everything</a></li>{{--<span id="span" style="font-size:small; left:47%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                                <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;" id="sub"data-toggle="tab" href="#subslist">Posts</a></li>{{--<span id="span" style="font-size: small; left:68%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                                <li role="presentation" id="dash"><a style="border-top-width: 0px;border-radius: 25px;border-left-width: 0px;"id="sub" data-toggle="tab" href="#category_list">Tags</a></li>{{--<span id="span" style="font-size: small; left:73%" class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>--}}
+                            </ul>
                         </div>
                     </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="tab-content">
-
-
-                            <div id="home" class="tab-pane fade in active">
-                                <div class="row">
-                                    <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;">
+                </div>
+                <div class="col-md-8 col-sm-8 {{--col-md-offset-1 col-sm-offset-1--}}">
+                    <div class="tab-content">
+                        <div id="home" class="tab-pane fade in active">
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;">
                                     <!-- Media middle -->
                                     <div class="media">
-
-                                        <div class="media-left media-middle">
-
-                                            <img src="images/vectors/12-512.png" class="media-object" style="width:150px;height:auto;">
-
-                                        </div>
-
-                                        <div class="media-body">
-
-                                            <h4 class="media-heading">Live Feeds</h4><b>Time Stamp</b> &nbsp;<a><b>Feeds</b></a>  
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-
-                                            <div>
-                                                <a><i class="fa fa-2x fa-thumbs-up"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-share"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-comment"></i></a>  &nbsp;&nbsp;
-
-                                            </div>
-
-
-                                        </div>
+                                        @include ('user.partials._postbox')
                                     </div>
                                     </div>
                                 </div>
                                 <hr style="border-top:5px solid #eee">
-                                <div class="row">
-                                    <div class="col-md-12 col-sm-12">
-                                        <div class="media">
-                                            <div class="media-left media-middle">
-                                                <img src="images/food.png"id="food" class="media-object img-rounded" style="width:150px;height:auto;">
-                                            </div>
-                                            <div class="media-body">
-                                                <h4 class="media-heading">Category</h4><b>food</b> &nbsp;<a><b>Tikka</b></a>  
-                                                <p>Food was amazing ! we will visit again.</p>
-                                                <div>
-                                                    <a><i class="fa fa-2x fa-thumbs-up"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                    <a><i class="fa fa-2x fa-share"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                    <a><i class="fa fa-2x fa-comment"></i></a>  &nbsp;&nbsp;
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {{--@include ('user.partials._postbox')--}}     
-
-                            <div id="category_list" class="tab-pane fade in fixed-bottom">
-
-                                <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;">
+                        </div>
+                            {{--@include ('user.partials._postbox')--}}
+                        <div id="category_list" class="tab-pane fade in fixed-bottom">
+                            <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;padding-left:0px;">
                                     <!-- Media middle -->
-                                    <div class="media">
-
-                                        <div class="media-left media-middle">
-
-                                            <img src="images/vectors/12-512.png" class="media-object" style="width:150px;height:auto;">
-
-                                        </div>
-
-                                        <div class="media-body">
-
-                                            <h4 class="media-heading">Tags</h4><b>Time Stamp</b> &nbsp;<a><b>Tags</b></a>  
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-
-                                            <div>
-                                                <a><i class="fa fa-2x fa-thumbs-up"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-share"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-comment"></i></a>  &nbsp;&nbsp;
-
-                                            </div>
-
-
-                                        </div>
-                                    </div>
+                                <div class="media">
+                                    @include ('user.partials._postbox')
                                 </div>
                             </div>
-
-
-
-                            <div id="subslist" class="tab-pane fade in">
-
-                                <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;">
+                        </div>
+                        <div id="subslist" class="tab-pane fade in">
+                            <div class="col-md-12 col-sm-12" style="margin-bottom: 30px;padding-left:0px;">
                                     <!-- Media middle -->
-                                    <div class="media">
-
-                                        <div class="media-left media-middle">
-
-                                            <img src="images/vectors/12-512.png" class="media-object" style="width:150px;height:auto;">
-
-                                        </div>
-
-                                        <div class="media-body">
-
-                                            <h4 class="media-heading">Posts</h4><b>Time Stamp</b> &nbsp;<a><b>Posts</b></a>  
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-
-                                            <div>
-                                                <a><i class="fa fa-2x fa-thumbs-up"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-share"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                                <a><i class="fa fa-2x fa-comment"></i></a>  &nbsp;&nbsp;
-
-                                            </div>
-
-
-                                        </div>
-                                    </div>
+                                <div class="media">
+                                    @include ('user.partials._postbox')
                                 </div>
                             </div>
-
-
-
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
+    </div>
 
    {{-- <div class="container">
         <div class="row">
